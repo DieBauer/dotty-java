5f130f7583add26d3ba904846db766cc33e3bb68
Jens Kat <jenskat@gmail.com>
2020-04-19 21:31:45 +0200

    Add minimal example

--- src/main/scala/Jersey.scala
+++ src/main/scala/Jersey.scala
@@ -12,16 +12,16 @@ import org.glassfish.jersey.server.internal.inject.{
 import org.glassfish.jersey.server.model.Parameter
 import org.glassfish.jersey.server.spi.internal.ValueParamProvider
 
-class MyFactoryProvider @Inject()(mpep: MultivaluedParameterExtractorProvider, locator: ServiceLocator)
-    extends AbstractValueParamProvider(
-    //   () => mpep,
-          new Provider[MultivaluedParameterExtractorProvider] {
-        override def get(): MultivaluedParameterExtractorProvider = mpep
-      },
-      org.glassfish.jersey.model.Parameter.Source.UNKNOWN
-    ) {
+// class MyFactoryProvider @Inject()(mpep: MultivaluedParameterExtractorProvider, locator: ServiceLocator)
+//     extends AbstractValueParamProvider(
+//     //   () => mpep,
+//           new Provider[MultivaluedParameterExtractorProvider] {
+//         override def get(): MultivaluedParameterExtractorProvider = mpep
+//       },
+//       org.glassfish.jersey.model.Parameter.Source.UNKNOWN
+//     ) {
 
-  override def createValueProvider(parameter: Parameter): Function[ContainerRequest, _] =
-  (request: ContainerRequest) => ""
+//   override def createValueProvider(parameter: Parameter): Function[ContainerRequest, _] =
+//   (request: ContainerRequest) => ""
 
-    }
+//     }
--- src/main/scala/Spring.scala
+++ src/main/scala/Spring.scala
@@ -1,6 +1,6 @@
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 // Spring mvc: Doesn't work in 0.23.0-RC1, works in Nightly 0.24.0
-class Handler extends ResponseEntityExceptionHandler
+// class Handler extends ResponseEntityExceptionHandler
 
 
